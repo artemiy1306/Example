423ec4133269aacfd9a2fdd1ba1bfcc292d5f3f0
artemiy <karelskiy@mail.ru>
2023-03-26 19:09:08 +0300

    add Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] array = { 1, 4, 31, 4, 15, 16, 17 ,18 };
+int n = array.Length;
+int find = 4;
+int index = 0;
+while (index < n){
+  if(array[index] == find)
+  {
+     Console.WriteLine(index);
+     break;
+  }
+  index++;
+}
+
