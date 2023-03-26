dd643285256436be72e03af85cbb479dae5b6095
artemiy <karelskiy@mail.ru>
2023-03-26 18:31:12 +0300

    add Еxample009_IntroArray

---
+++ Еxample009_IntroArray/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Max(int arg1, int arg2, int arg3){
+    int result = arg1;
+    if(arg2>result) result=arg2;
+    if(arg3>result) result=arg3;
+    return result;
+}
+int[] array = { 1, 12, 31, 4, 153, 16, 17, 18, 57 };
+
+int result = Max(
+  Max(array[0], array[1], array[2]),
+  Max(array[3], array[4], array[5]),
+  Max(array[6], array[7], array[8])
+);
+Console.WriteLine(result);  
